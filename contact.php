5c9974a872ed7df4deba5e27f6be5c760411acf7
Rajesh <80874249+RajeshHsejar27@users.noreply.github.com>
2022-05-15 22:45:52 +0530

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>EMS</title>
+        <?php require 'utils/styles.php'; ?><!--css links. file found in utils folder-->
+        <?php require 'utils/scripts.php'; ?><!--js links. file found in utils folder-->
+    </head>
+    <body>
+        <?php require 'utils/header.php'; ?><!--header content. file found in utils folder-->
+        <div class = "content"><!--body content holder-->
+            <div class = "container">
+                <div class = "col-md-12"><!--body content title holder with 12 grid columns-->
+                    <h1>Contact Us</h1><!--body content title-->
+                </div>
+            </div>
+			
+            <div class="container">
+            <div class="col-md-12">
+            <hr><div class="container">
+               <div class="powr-contact-form" label=""></div>
+            </div>
+            </div>
+            </div>
+            
+              <div class="powr-form-builder" id="d999c4cd_1652633542"></div><script src="https://www.powr.io/powr.js?platform=html"></script>
+			
+            
+        </div><!--body content div-->
+        <?php require 'utils/footer.php'; ?><!--footer content. file found in utils folder-->
+      
+    </body>
+</html>
---
+++ utils/footer.php
@@ -0,0 +1,35 @@
+<hr class = "footerline"><!--css modified horizontal line-->
+<footer>
+    <div class = "container">
+        <div class = "row">
+            <section>
+                <div class = "footerContent col-md-4"><!--left content-->
+                    <p class = "footerContent1">
+                        <strong>E</strong><span class = "small footerSubtext">vent</span>
+                        <strong>M</strong><span class = "small footerSubtext">anagers</span>
+                        <strong>I</strong><span class = "small footerSubtext">n</span>
+                        <strong>A</strong><span class = "small footerSubtext">ction.</span>
+                    </p>
+
+                    <p class = "footerSubtext2">
+                        Imaginary Building,soon to be built,
+                        &copy; Urban Events Venue & Catering, 2022.
+                    </p>
+                </div>
+            </section>
+            <section>
+                <div class = "footcontent col-md-4"><!--middle content-->
+                    Imaginary street<br>
+                    EMS 2022.<br>
+                    India, 000000
+                </div>
+            </section>
+            <section>
+                <div class = "footcontent col-md-4"><!--right content-->
+                    Follow Us:<br>
+                       <a href="https://www.facebook.com/Event-Management-111783728161921/"> <img src = "images/facebook.png"> </a>
+                </div>
+            </section>
+        </div>
+    </div>
+</footer>
